5acde3f9f3286beefb181c062adfbe7ad67a1b91
nan2xiao <3134955652@qq.com>
2024-04-30 15:29:56 +0800

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -4,10 +4,8 @@ var audio = document.querySelector('.music-player audio');
 // 设置默认音量为20%
 audio.volume = 0.2;
 
-// 当音频播放完毕时，等待3秒后重新播放
+// 当音频播放完毕时，等待4秒后重新播放
 audio.addEventListener('ended', function() {
-    setTimeout(function() {
-        audio.currentTime = 0; // 重置播放时间到开始
-        audio.play(); // 重新播放
-    }, 3000); // 3秒后重新播放
-}, 3000);
+    audio.currentTime = 0; // 重置播放时间到开始
+    audio.play(); // 重新播放
+}, 4000);
