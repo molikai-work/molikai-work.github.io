98e6717f290fa95731289cc99fc95412bf62bfdf
nan2xiao <3134955652@qq.com>
2024-04-30 15:20:06 +0800

    Update .

---
+++ js/audio.js
@@ -0,0 +1,11 @@
+// 获取音频元素
+var audio = document.querySelector('.music-player audio');
+
+// 设置默认音量为20%
+audio.volume = 0.2;
+
+// 当音频播放完毕时，重新播放
+audio.addEventListener('ended', function() {
+    audio.currentTime = 0; // 重置播放时间到开始
+    audio.play(); // 重新播放
+});
